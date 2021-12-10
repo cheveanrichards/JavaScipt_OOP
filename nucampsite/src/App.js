6a5f2cc154b3d45813ded1b696331d1840959efb
cheveanrichards <richardsca7@outlook.com>
2021-12-09 22:26:40 -0500

    Creating a react component using higher order map function

---
+++ nucampsite/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Directory from './components/DirectoryComponent';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Navbar dark color="primary">
+                <div className="container">
+                    <NavbarBrand href="/">NuCamp</NavbarBrand>
+                </div>
+                </Navbar>
+                <Directory/>
+            </div>
+        );
+    }
+}
+
+export default App;
