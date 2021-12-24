48656252f6e4f7a75ffbfedf236b1bc27a965278
cheveanrichards <richardsca7@outlook.com>
2021-12-23 19:17:26 -0500

    implementing the react router adding the path to the Contact

--- nucampsite/src/components/MainComponent.js
+++ nucampsite/src/components/MainComponent.js
@@ -3,6 +3,12 @@ import Directory from './DirectoryComponent';
 import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import {Switch, Route, Redirect } from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 import CampsiteInfo from './CampsiteInfoComponent';
 
 class Main extends Component {
@@ -10,20 +16,30 @@ class Main extends Component {
       super(props);
       this.state = {
         campsites: CAMPSITES,
-        selectedCampsite: null
+        comments: COMMENTS,
+        partners: PARTNERS,
+        promotions: PROMOTIONS,
       }
     }
-
-    onCampsiteSelect(campsiteId){
-        this.setState({selectedCampsite: campsiteId});
-    }
-
     render() {
+        const HomePage = () => {
+            return(
+                <Home
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
+            )
+        }
         return (
             <div>
                 <Header/>
-                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/>
+                <Switch>
+                    <Route path='/home' component={HomePage}/>
+                    <Route exact path='/directory' render={()=><Directory campsites={this.state.campsites}/>}/>
+                    <Route exact path='/contactus' component={Contact}/>
+                    <Redirect to='/home'/>
+                </Switch>
                 <Footer/>
             </div>
         );
